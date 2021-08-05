f01bd6428a59a1cfff3b8a294b2548589ba5d0db
DY <davidyue1@gmail.com>
2021-08-05 01:37:16 -0500

    submission pages added

--- server/static/js/submit.js
+++ server/static/js/submit.js
@@ -9,3 +9,36 @@ class SubmitSubtask {
     return { data };
   }
 }
+
+
+$(function() {
+  $('#submit-button').click(function () {
+
+    const params = new URLSearchParams(window.location.search)
+
+    const hitId = params.get("hitId")
+    const workerId = params.get("workerId")
+    const assignmentId = params.get("assignmentId")
+    const submitUrl = params.get("turkSubmitTo") + '/mturk/externalSubmit'
+
+    
+    var form = $("#submit-form");
+    // addHiddenField(form, "hitId", hitId);
+    // addHiddenField(form, "workerId", workerId);
+    // addHiddenField(form, "assignmentId", assignmentId);
+
+
+    var results = {
+      data: this.data,
+    };
+
+
+    console.log(results);
+
+    // addHiddenField(form, "results", JSON.stringify(results));
+
+    // $("#submit-form").attr("action", submitUrl);
+    // $("#submit-form").attr("method", "POST");
+    // $("#submit-form").submit();
+  });
+});
